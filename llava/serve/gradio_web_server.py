34729b7f0711232ab5fd5b8ec4bf8d5aef8891a3
Ronald Seoh <ronaldseoh@icloud.com>
2024-05-26 16:22:38 +0900

    debug

--- llava/serve/gradio_web_server.py
+++ llava/serve/gradio_web_server.py
@@ -380,9 +380,9 @@ def http_bot_nostream(state, model_selector, temperature, top_p, max_new_tokens,
 
     try:
         response = requests.post(worker_addr + "/worker_generate_nostream",
-            headers=headers, json=pload, stream=False, timeout=10)
+            headers=headers, json=pload, stream=False, timeout=200)
 
-        logger.info(response.text)
+        logger.warning(response.text)
 
         for chunk in response.iter_lines(decode_unicode=False, delimiter=b"\0"):
             if chunk:
