0ed4bb079f5eb867a143f823776fcbfee8c4023d
Ronald Seoh <ronaldseoh@icloud.com>
2024-05-27 08:58:54 +0900

    minor

--- llava/serve/model_worker.py
+++ llava/serve/model_worker.py
@@ -306,7 +306,7 @@ class ModelWorker:
 
     def generate_nostream_gate(self, params):
         try:
-            return ORJSONResponse(self.generate_nostream(params))
+            return self.generate_nostream(params)
         except ValueError as e:
             print("Caught ValueError:", e)
             ret = {
@@ -314,7 +314,7 @@ class ModelWorker:
                 "log_probs": [],
                 "error_code": 1,
             }
-            return ORJSONResponse(ret)
+            return ret
         except torch.cuda.CudaError as e:
             print("Caught torch.cuda.CudaError:", e)
             ret = {
@@ -322,7 +322,7 @@ class ModelWorker:
                 "log_probs": [],
                 "error_code": 1,
             }
-            return ORJSONResponse(ret)
+            return ret
         except Exception as e:
             print("Caught Unknown Error", e)
             ret = {
@@ -330,7 +330,7 @@ class ModelWorker:
                 "log_probs": [],
                 "error_code": 1,
             }
-            return ORJSONResponse(ret)
+            return ret
 
 app = FastAPI()
 
@@ -372,7 +372,7 @@ async def generate_nostream(request: Request):
     background_tasks = BackgroundTasks()
     background_tasks.add_task(partial(release_model_semaphore, fn=worker.send_heart_beat))
 
-    return output
+    return ORJSONResponse(output)
 
 
 @app.post("/worker_get_status")
