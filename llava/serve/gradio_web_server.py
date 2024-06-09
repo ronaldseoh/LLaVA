eed3b84726dc8b84dc2f8f85fc4bc32c77e312fc
Ronald Seoh <ronaldseoh@icloud.com>
2024-06-09 10:48:05 +0900

    llava/serve/gradio_web_server.py: remove_last_conversation()

--- llava/serve/gradio_web_server.py
+++ llava/serve/gradio_web_server.py
@@ -125,6 +125,17 @@ def clear_history(request: gr.Request):
     return (state, state.to_gradio_chatbot(), "", None) + (disable_btn,) * 5
 
 
+def remove_last_conversation(state, request: gr.Request):
+    logger.info(f"remove_last_conversation. ip: {request.client.host}")
+
+    # state is actually in a fresh state, just return as-is
+    if len(state.messages) == state.offset + 2:
+        return (state, state.to_gradio_chatbot(), "", None) + (disable_btn,) * 5
+    else:
+        del state.messages[-1]
+        del state.messages[-1]
+
+
 def add_text(state, text, image, image_process_mode, request: gr.Request):
     logger.info(f"add_text. ip: {request.client.host}. len: {len(text)}")
     if len(text) <= 0 and image is None:
